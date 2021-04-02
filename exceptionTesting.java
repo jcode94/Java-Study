5856596873353626b39c8048a888a2454b38e8b6
Justice Smith <jcode94ttv@gmail.com>
2021-04-02 01:24:42 -0400

    started light exception handling testing for Exam 2

---
+++ exceptionTesting.java
@@ -0,0 +1,13 @@
+/*
+class Main {
+  public static void main(String args[]) {
+    try {
+      throw 10; 
+    } 
+    catch(int e) {
+      System.out.println("Got the Exception "+ e); 
+    }
+  }
+}
+*/
+
